5c0c7b3bf1b51b3f00acbe355cea0389a4ec7986
Pawan Paudel <pawanpaudel93@gmail.com>
2023-05-31 22:05:20 +0545

    feat: add HtmlScreenshotSaver class

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,217 @@
-export const one = 1
-export const two = 2
+import { promises as fsPromises } from 'node:fs'
+import path from 'node:path'
+import type { Buffer } from 'node:buffer'
+import { findChrome } from 'find-chrome-bin'
+import { execFile } from 'promisify-child-process'
+import { temporaryDirectory } from 'tempy'
+import { runBrowser } from './single-file'
+
+export interface SaveReturnType {
+  status: 'success' | 'error'
+  message: string
+  webpage: string
+  screenshot: string
+  title: string
+  timestamp: number
+}
+
+interface BrowserlessOptions {
+  apiKey: string
+  proxyServer?: string
+  blockAds?: boolean
+  stealth?: boolean
+  userDataDir?: string
+  keepalive?: number
+  windowSize?: string
+  ignoreDefaultArgs?: string
+  headless?: boolean
+  userAgent?: string
+  timeout?: number
+}
+
+const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+
+export class HtmlScreenshotSaver {
+  private browserServer?: string
+
+  constructor(browserlessOptions?: BrowserlessOptions) {
+    this.processBrowserless(browserlessOptions)
+  }
+
+  private processBrowserless(options?: BrowserlessOptions) {
+    if (options?.apiKey)
+      this.browserServer = this.constructBrowserlessUrl(options)
+  }
+
+  private constructBrowserlessUrl(options: BrowserlessOptions): string {
+    const {
+      apiKey,
+      proxyServer,
+      blockAds,
+      stealth,
+      userDataDir,
+      keepalive,
+      windowSize,
+      ignoreDefaultArgs,
+      headless,
+      userAgent,
+      timeout,
+    } = options
+
+    let url = `wss://chrome.browserless.io/?token=${apiKey}`
+
+    if (proxyServer)
+      url += `&--proxy-server=${proxyServer}`
+
+    if (blockAds)
+      url += '&blockAds'
+
+    if (stealth)
+      url += '&stealth'
+
+    if (userDataDir)
+      url += `&--user-data-dir=${userDataDir}`
+
+    if (keepalive)
+      url += `&keepalive=${keepalive}`
+
+    if (windowSize)
+      url += `&--window-size=${windowSize}`
+
+    if (ignoreDefaultArgs)
+      url += `&ignoreDefaultArgs=${ignoreDefaultArgs}`
+
+    if (headless !== undefined)
+      url += `&headless=${headless}`
+
+    if (timeout)
+      url += `&timeout=${timeout}`
+
+    url += `&user-agent=${userAgent || USER_AGENT}`
+
+    return url
+  }
+
+  private async readFileAsBuffer(filePath: string): Promise<Buffer> {
+    return await fsPromises.readFile(filePath)
+  }
+
+  private async getChromeExecutablePath() {
+    const { executablePath } = await findChrome({})
+    return executablePath
+  }
+
+  private getErrorMessage(error: unknown): string {
+    if (
+      typeof error === 'object'
+    && error !== null
+    && 'message' in error
+    && typeof (error as Record<string, unknown>).message === 'string'
+    )
+      return (error as { message: string }).message
+
+    try {
+      return new Error(JSON.stringify(error)).message
+    }
+    catch {
+      return String(error)
+    }
+  }
+
+  private async runBrowser({
+    browserArgs,
+    browserExecutablePath,
+    url,
+    basePath,
+    output,
+  }: {
+    browserArgs: string
+    browserExecutablePath: string
+    url: string
+    basePath: string
+    output: string
+  }) {
+    try {
+      await runBrowser({
+        browserArgs,
+        browserExecutablePath,
+        url,
+        basePath,
+        output,
+        userAgent: USER_AGENT,
+        browserServer: this.browserServer,
+      })
+    }
+    catch (error) {
+      const command = [
+        `--browser-executable-path=${browserExecutablePath}`,
+        `--browser-args='${browserArgs}'`,
+        url,
+        `--output=${output}`,
+        `--base-path=${basePath}`,
+        `--user-agent=${USER_AGENT}`,
+        `--browser-server=${this.browserServer}`,
+      ]
+      await execFile('./node_modules/single-file-cli/single-file', command)
+    }
+  }
+
+  public save = async (url: string, folderPath?: string): Promise<SaveReturnType> => {
+    try {
+      if (!folderPath)
+        folderPath = temporaryDirectory()
+
+      await fsPromises.stat(folderPath)
+
+      try {
+        await this.runBrowser({
+          browserArgs:
+            '["--no-sandbox", "--window-size=1920,1080", "--start-maximized"]',
+          browserExecutablePath: await this.getChromeExecutablePath(),
+          url,
+          basePath: folderPath,
+          output: path.resolve(folderPath, 'index.html'),
+        })
+      }
+      catch (error) {
+        return {
+          status: 'error',
+          message: this.getErrorMessage(error),
+          webpage: '',
+          screenshot: '',
+          title: '',
+          timestamp: 0,
+        }
+      }
+
+      const metadata: {
+        title: string
+        url: string
+      } = JSON.parse(
+        (
+          await this.readFileAsBuffer(path.join(folderPath, 'metadata.json'))
+        ).toString(),
+      )
+      const timestamp = Math.floor(Date.now() / 1000)
+
+      return {
+        status: 'success',
+        message: '',
+        webpage: path.join(folderPath, 'index.html'),
+        screenshot: path.join(folderPath, 'screenshot.png'),
+        title: metadata.title,
+        timestamp,
+      }
+    }
+    catch (error) {
+      return {
+        status: 'error',
+        message: this.getErrorMessage(error),
+        webpage: '',
+        screenshot: '',
+        title: '',
+        timestamp: 0,
+      }
+    }
+  }
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,14 @@
+import fsPromises from 'node:fs/promises'
 import { describe, expect, it } from 'vitest'
+import { HtmlScreenshotSaver } from '../src'
 
 describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
-  })
+  it('save url html and screenshot', async () => {
+    const saver = new HtmlScreenshotSaver()
+    const result = await saver.save('https://github.com/pawanpaudel93')
+    // eslint-disable-next-line no-console
+    console.log(result)
+    expect(result.status).toEqual('success')
+    await fsPromises.rm(result.webpage.replace('index.html', ''), { recursive: true, force: true })
+  }, 60000)
 })
